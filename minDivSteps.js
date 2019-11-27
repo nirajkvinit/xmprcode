8721d6a399588c80e34574766606aeec18fb8ca0
Niraj Kumar <nirajkvinit@gmail.com>
2019-11-27 11:45:53 +0530

    Updated minDiv file
    Added another condition to breakout from the loop early. Apprantly if a resulting number becomes less than 3 then no further processing will result in 0.

--- minDivSteps.js
+++ minDivSteps.js
@@ -5,22 +5,25 @@ const minDivSteps = inputNum => {
     return notPossible;
   }
 
-  // if number is not divisable by 3 then getting 0 is not possible
-  if (inputNum % 3 !== 0) {
-    return notPossible;
-  }
+  // Going by the pattern, it has been deduced that only those numbers
+  // which are divisable by 3 can become 0
+  // So, we can uncomment the code below to save some operation time.
+  // if (inputNum % 3 !== 0) {
+  //   return notPossible;
+  // }
 
   let steps = 0;
-  let result = inputNum;
 
-  while (result !== 0) {
-    if (result % 2 === 0) {
-      // if number is even then divide by 2
-      result = result / 2;
-    } else {
-      // else substract 3 from it
-      result = result - 3;
+  while (inputNum !== 0) {
+    if (inputNum < 3) {
+      // if inputNum becomes less than 3 then return 'Not possible'
+      return notPossible;
     }
+
+    // if inputNum is even then divide it by 2
+    // else substract 3 from it
+
+    inputNum = inputNum % 2 === 0 ? inputNum / 2 : inputNum - 3;
 
     // for every operation increase the steps with 1
     steps++;
@@ -31,9 +34,15 @@ const minDivSteps = inputNum => {
 
 // Execution testing
 // (() => {
+//   let possibleCounter = 0;
 //   for (let i = 0; i < 200; i++) {
-//     console.log(`${i} : ${minDivSteps(i)}, `);
+//     let result = minDivSteps(i);
+//     console.log(`${i} : ${result}, `);
+//     if (Number.isInteger(result)) {
+//       possibleCounter++;
+//     }
 //   }
+//   console.log(`Total possible ${possibleCounter}`);
 // })();
 
 module.exports = minDivSteps;
